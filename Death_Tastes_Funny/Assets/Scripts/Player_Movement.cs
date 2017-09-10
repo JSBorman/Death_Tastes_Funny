bd816ee33a4a52b02a5613d91e581e1d42193a61
Staethe <jzmynb@gmail.com>
2017-09-10 18:03:30 -0400

    Setup SuperCollider things

---
+++ Death_Tastes_Funny/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float move_speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		get_input ();
+	}
+
+	public void get_input(){
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * move_speed;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
+
+		var old_pos = transform.position;
+		transform.Translate (x, y, 0);
+
+		//OSC SEND
+		if (old_pos != transform.position) {
+			float move = 1.0f;
+			OSCHandler.Instance.SendMessageToClient ("SuperCollider", "/move", move);
+		}
+	}
+}
