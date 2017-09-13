bdd78008184affa33c6ac5db50c4e099e0659e87
Harrison Essig <harrisonessig@gmail.com>
2017-09-13 17:08:37 -0400

    Merge

---
+++ Death_Tastes_Funny/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 1, -1));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+}
