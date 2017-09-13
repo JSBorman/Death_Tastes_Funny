699b7eee87744817d0cf4b18bec59134b9d59577
Dan Gorman <danthemangorman@gmail.com>
2017-09-13 16:33:17 -0400

    made the shapes the right shapes

--- Death_Tastes_Funny/Assets/Scripts/ShapeShifter.cs
+++ Death_Tastes_Funny/Assets/Scripts/ShapeShifter.cs
@@ -20,23 +20,34 @@ public class ShapeShifter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (GameManager.INSTANCE.Debug) {
-            if (Input.GetKeyDown(KeyCode.E)) {
+            if (Input.GetKeyDown(KeyCode.Q)) {
+                SetShape(0);
+            }
+            else if (Input.GetKeyDown(KeyCode.W)) {
+                SetShape(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.E)) {
                 SetShape(2);
-            } else if (Input.GetKeyDown(KeyCode.R)) {
+            }
+            else if (Input.GetKeyDown(KeyCode.R)) {
                 SetShape(3);
-            } else if (Input.GetKeyDown(KeyCode.Y)) {
+            }
+            else if (Input.GetKeyDown(KeyCode.T)) {
                 SetShape(4);
-            } else if (Input.GetKeyDown(KeyCode.I)) {
+            }
+            else if (Input.GetKeyDown(KeyCode.Y)) {
                 SetShape(5);
+            }
+            else if (Input.GetKeyDown(KeyCode.U)) {
+                SetShape(6);
+            }
+            else if (Input.GetKeyDown(KeyCode.I)) {
+                SetShape(7);
             }
         }
 	}
 
     public void SetShape(int level) {
-        if (switching) {
-            return;
-        }
-        switching = true;
         if (level < 0 || level >= shapes.Length) {
             Debug.LogError(string.Format("{0} is not a valid shape level",level));
             return;
@@ -44,6 +55,10 @@ public class ShapeShifter : MonoBehaviour {
             Debug.LogError(string.Format("There is no sprite for shape level {0}", level));
             return;
         }
+        if (switching) {
+            return;
+        }
+        switching = true;
         Debug.Log(string.Format("Changing to level {0}...", level));
         SpriteRenderer oldRenderer = activeRenderer;
         activeRenderer = oldRenderer == renderers[0] ? renderers[1] : renderers[0];
@@ -71,7 +86,7 @@ public class ShapeShifter : MonoBehaviour {
 
     IEnumerator Grow(SpriteRenderer r) {
         Vector3 scale = Vector3.zero;
-        Vector3 position = new Vector3(0, -.5f, 0);
+        Vector3 position = new Vector3(0, -2f, 0);
         r.gameObject.transform.localScale = scale;
         r.gameObject.transform.localPosition = position;
         r.color = new Color(r.color.r, r.color.g, r.color.b, 1);
@@ -80,7 +95,7 @@ public class ShapeShifter : MonoBehaviour {
         while (t <= fadeTime) {
             t += Time.deltaTime;
             scale.x = scale.y = scale.z = t / fadeTime;
-            position.y = -.5f + t / (2*fadeTime);
+            position.y = -2f + 2*t / (fadeTime);
             r.gameObject.transform.localScale = scale;
             r.gameObject.transform.localPosition = position;
             yield return null;
