fb883fa0bd80cfa1ccb72a430e246b8249e611c9
Harrison Essig <harrisonessig@gmail.com>
2017-09-15 16:48:07 -0400

    Colors and Level stuff
    -Made things colorful, haven't added background yet
    -use gradient to make the bottom to top teleporting less confusing
    -fancy hidden blocks to make people want to die
    -probably more things, I don't remember

---
+++ Death_Tastes_Funny/Assets/Scripts/Level Scripts/VisibleOnCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleOnCollision : MonoBehaviour {
+
+    public float alphaOnCollision = 1;
+
+	// Use this for initialization
+	void Start () {
+        Color tmp = GetComponent<SpriteRenderer>().color;
+        tmp.a = 0;
+        GetComponent<SpriteRenderer>().color = tmp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color;
+        tmp.a = alphaOnCollision;
+        GetComponent<SpriteRenderer>().color = tmp;
+
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color;
+        tmp.a = 0;
+        GetComponent<SpriteRenderer>().color = tmp;
+
+    }
+}
