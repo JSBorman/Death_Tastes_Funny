40d87a2fcb0c14a7b1bf5aea1d5bb7e930b53f95
Staethe <jzmynb@gmail.com>
2017-09-19 22:07:16 -0400

    More more on backgrounds

--- Death_Tastes_Funny/Assets/Scripts/Background_Translation.cs
+++ Death_Tastes_Funny/Assets/Scripts/Background_Translation.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Background_Translation : MonoBehaviour {
 
-	public GameObject player;
-	public Camera camera;
+	private GameObject player;
+	private Camera camera;
 
 	//Parallaxing variables
 	public float par_upper_bound = 100f;
@@ -16,14 +16,19 @@ public class Background_Translation : MonoBehaviour {
 	public float squa_upper_bound = 2.0f;
 	public float squa_lower_bound = 0.5f;
 	public float squa_move_rate = 1.0f;
+	public List<int> squa_allowed_formats = new List<int> {1, 1, 1};	//All formats allowed
 	private float squa_transform = 1.0f;
+	private float squa_time_since_format = 1.0f;	//1 second between direction changes
 	private int squa_toShrink = 1;
+	private int squa_format = 0;
 
 	Vector3 player_last_pos;
 
 	// Use this for initialization
 	void Start () {
-			
+		player = GameObject.FindGameObjectWithTag ("Player");
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+
 	}
 	
 	// Update is called once per frame
@@ -43,6 +48,7 @@ public class Background_Translation : MonoBehaviour {
 	//Move up & down as the player walks by
 	public void parralaxing(){
 		float new_pos = 0.0f;
+		squa_time_since_format -= Time.deltaTime; //Update time
 
 		//Player moved left
 		if (player_last_pos.x >= player.transform.position.x && this.transform.position.y > par_lower_bound) {
@@ -61,7 +67,7 @@ public class Background_Translation : MonoBehaviour {
 		player_last_pos = player.transform.position;
 	}
 
-	//Over time, stretch and squish the object
+	//Over time, stretch and squish the object over different vertices
 	public void squashing(){
 
 		if (squa_transform > squa_upper_bound) {
@@ -76,6 +82,30 @@ public class Background_Translation : MonoBehaviour {
 		float change = squa_move_rate * Time.deltaTime * squa_toShrink;
 		squa_transform += change;
 
-		this.transform.localScale += new Vector3 (change, change, 0);
+		//If timer ran out
+		if (squa_time_since_format <= 0) {
+			squa_time_since_format = 1.0f;
+			squa_format = Random.Range (0, 2);
+		}
+
+		//If selected format is not allowed, stop moving
+		if (squa_allowed_formats[squa_format] == 0)
+			squa_format = 0;
+
+		//Alter scale of planet based on current orientation
+		switch (squa_format) {
+			case 0:
+				this.transform.localScale += new Vector3 (change, change, change);
+				break;	
+			case 1:
+				this.transform.localScale += new Vector3 (0, change, 0);
+				break;
+			case 2:
+				this.transform.localScale += new Vector3 (change, 0, 0);
+				break;
+			default:
+				this.transform.localScale += new Vector3 (0, 0, 0);
+				break;
+		}
 	}
 }
