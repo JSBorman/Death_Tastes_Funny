184612e79679f5a551f6e233b21e703cf819ff2a
Staethe <jzmynb@gmail.com>
2017-09-19 21:03:32 -0400

    Moved player lower in viewport

--- Death_Tastes_Funny/Assets/Scripts/CameraFollowPlayer.cs
+++ Death_Tastes_Funny/Assets/Scripts/CameraFollowPlayer.cs
@@ -15,7 +15,7 @@ public class CameraFollowPlayer : MonoBehaviour {
     private Vector3 velocity = Vector3.zero;
     void Update()
     {
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 1, -1));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -1));
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 
